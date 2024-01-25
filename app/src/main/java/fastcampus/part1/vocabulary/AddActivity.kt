e3f4c3e642432f80c9f7e9ae03766a22b651053f
1three <1_three@naver.com>
2024-01-25 17:55:17 +0900

    feat: 8품사 Chip 구현

--- app/src/main/java/fastcampus/part1/vocabulary/AddActivity.kt
+++ app/src/main/java/fastcampus/part1/vocabulary/AddActivity.kt
@@ -2,10 +2,37 @@ package fastcampus.part1.vocabulary
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.chip.Chip
+import fastcampus.part1.vocabulary.databinding.ActivityAddBinding
 
 class AddActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityAddBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add)
+        binding = ActivityAddBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initViews()
+    }
+
+    private fun initViews() {
+        val types = listOf("명사", "대명사", "동사", "형용사", "부사", "감탄사", "전치사", "접속사")
+        binding.typeChipGroup.apply {
+            types.forEach { text ->
+                addView(createChip(text))
+            }
+        }
+    }
+
+    private fun createChip(text: String): Chip {
+        return Chip(
+            this,
+            null,
+            com.google.android.material.R.style.Widget_MaterialComponents_Chip_Choice
+        ).apply {
+            setText(text)
+            isCheckable = true
+            isClickable = true
+        }
     }
 }
